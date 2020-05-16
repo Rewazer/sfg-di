0e5db72ae3488a84a2b819c41a68399a36a2f55e
sstakheev <sstakheev@linius.com>
2020-05-16 19:31:51 +0500

    Add Property Dependency Injection Example

---
+++ src/main/java/ru/hcc/sfgdi/controllers/MyController.java
@@ -0,0 +1,11 @@
+package ru.hcc.sfgdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello() {
+        System.out.println("Hello world!!!");
+        return "Hi Folks";
+    }
+}
